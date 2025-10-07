9a5a4bf4263ac1be1e1bde2941c4d092ca393c8d
LMLTech <leminhliem.it@gmail.com>
2025-10-07 13:36:08 +0700

    Remove duplicate or corrupted project folders

--- learning2-uth-lml/src/main/java/edu/uth/model/Admin.java
+++
@@ -1,7 +0,0 @@
-package edu.uth.model;
-
-public class Admin extends User {
-    public Admin(String userId, String name, String email, String password) {
-        super(userId, name, email, password, "ADMIN");
-    }
-}
--- learning2-uth-lml/src/main/java/edu/uth/model/Trainer.java
+++
@@ -1,9 +0,0 @@
-package edu.uth.model;
-
-public class Trainer extends User{
-
-    public Trainer(String userId, String name, String email, String password) {
-        super(userId, name, email, password, "TRAINER");
-    }
-    
-}
